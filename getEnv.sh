a8005d5955064796f9cc6d5c867e09db2c615cf9
ken3pei <kensuke1202left@gmail.com>
2024-01-22 01:21:38 +0900

    env設定スクリプトを修正

--- getEnv.sh
+++ getEnv.sh
@@ -3,9 +3,15 @@
 aws secretsmanager get-secret-value --secret-id sbcntr/mysql \
 | jq -r .SecretString \
 | { read ss ; \
-    echo -e "$ss"| export DB_USERNAME="$(jq -r .username)"; \
-    echo -e "$ss"| export DB_PASSWORD="$(jq -r .password)"; \
-    echo -e "$ss"| export DB_CLIENT="$(jq -r .engine)"; \
-    echo -e "$ss"| export DB_HOST="$(jq -r .host)"; \
-    echo -e "$ss"| export DB_PORT="$(jq -r .port)"; \
+    echo -e "$ss"| DB_USERNAME="$(jq -r .username)"; \
+    echo -e "$ss"| DB_PASSWORD="$(jq -r .password)"; \
+    echo -e "$ss"| DB_CLIENT="$(jq -r .engine)"; \
+    echo -e "$ss"| DB_HOST="$(jq -r .host)"; \
+    echo -e "$ss"| DB_PORT="$(jq -r .port)"; \
 }
+
+echo $DB_USERNAME
+echo $DB_PASSWORD
+echo $DB_CLIENT
+echo $DB_HOST
+echo $DB_PORT
